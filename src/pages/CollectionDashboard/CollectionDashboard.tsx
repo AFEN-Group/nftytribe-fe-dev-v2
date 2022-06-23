3dc0ddffa443de94359ef8fc226eb62390a1a91e
AyoNiyi <mac@Ayos-Mac.local>
2022-06-23 19:53:52 +0100

    update: collection dashboard page styles

--- src/pages/CollectionDashboard/CollectionDashboard.tsx
+++ src/pages/CollectionDashboard/CollectionDashboard.tsx
@@ -17,6 +17,14 @@ const CollectionDashboard = () => {
   const [themeState] = useContext<any>(ThemeContext)
   const dark = themeState.dark
   let itemNumber = 1
+  const [filter, setFilter] = useState({
+    period: '',
+    chain: ''
+  })
+  const [showDrop, setShowDrop] = useState({
+    period: false,
+    chain: false
+  })
 
   const [collections, setCollections] = useState([])
   useEffect(() => {
@@ -70,23 +78,53 @@ const CollectionDashboard = () => {
                   Top Nfts are ranked according to Volume and floor price.
                 </span>
               </p>
+
             </div>
-            <div
-              className={`${style.filters} animate__animated animate__fadeInUp animate__delay-1s`}
-            >
-              {/* <div className={style.filter}>
+            <div className={style.filtersContainer}>
+
+
+              <div
+                className={`${style.filters} animate__animated animate__fadeInUp animate__delay-1s`}
+              >
+
+                {/* <div className={style.filter}>
                 <p>Categories</p>
                 <img src={arrowA} alt="categories" />
               </div> */}
-              <div className={style.filter}>
-                <p>Chains</p>
-                <img src={arrowA} alt="categories" />
+                <div className={style.filter} onClick={() => setShowDrop({ ...showDrop, chain: !showDrop.chain })}>
+                  <p>{filter.chain === "" ? 'Chains' : filter.chain}</p>
+                  <img src={arrowA} alt="categories" />
+
+                </div>
+
+                <div className={style.filter} onClick={() => setShowDrop({ ...showDrop, period: !showDrop.period })}>
+                  <p>{filter.period === "" ? 'Last 24 hours' : filter.period}</p>
+                  <img src={arrowA} alt="categories" />
+                </div>
+
               </div>
-              <div className={style.filter}>
-                <p>24 hours</p>
-                <img src={arrowA} alt="categories" />
-              </div>
+              {showDrop.chain && (
+                <div className={`${style.drop} ${dark === 'true' ? 'darkTheme' : 'lightTheme'} animate__animated animate__fadeInUp animate__faster`}
+                  onClick={() => setShowDrop({ ...showDrop, chain: !showDrop.chain })}
+                >
+                  <p onClick={() => setFilter({ ...filter, chain: "Eth" })}>Ethereum</p>
+                  <p className='disable_link2'>Binance</p>
+                  <p className='disable_link2'>Skale</p>
+                  <p className='disable_link2'>Solana</p>
+                </div>
+              )}
+              {showDrop.period && (
+                <div className={`${style.drop2} ${dark === 'true' ? 'darkTheme' : 'lightTheme'} animate__animated animate__fadeInUp animate__faster`}
+                  onClick={() => setShowDrop({ ...showDrop, period: !showDrop.period })}
+                >
+                  <p onClick={() => setFilter({ ...filter, period: "Last 24 hours" })}>Last 24 hours</p>
+                  <p onClick={() => setFilter({ ...filter, period: "Last 7 days" })}>Last 7 days</p>
+                  <p onClick={() => setFilter({ ...filter, period: "Last month" })}>Last month</p>
+
+                </div>
+              )}
             </div>
+
             <div
               className={`${style.topProTable} animate__animated animate__fadeInUp animate__delay-2s`}
             >
@@ -209,6 +247,69 @@ const CollectionDashboard = () => {
 
                 <div className={style.line1}></div>
               </div>
+            </div>
+
+            <div className={style.topProContainer}>
+              {/* <div className={style.tpItem}>
+                            <div className={style.tpLeft}>
+                                <p>{itemNumber++}</p>
+                                <img src={user} alt="user" />
+                            </div>
+                            <div className={style.tpRight}>
+                                <div className={style.tprInfo}>
+                                    <h2>Skimming cat</h2>
+                                    <p>Floor price: 1Eth</p>
+                                </div>
+                                <div className={style.tprNumbers}>
+                                    <p>+30</p>
+                                    <p>Vol: 6</p>
+                                </div>
+                            </div>
+
+                        </div> */}
+
+              {!collections
+                ? null
+                : collections.map((collection: any, i) => {
+                  return (
+                    collection.title && (
+                      <Link
+                        to={`/collectionDetails/${collection.contract_address}`}
+                        className={style.tpItem}
+                        key={collection._id}
+                      >
+                        <div className={style.tpLeft}>
+                          <p>{itemNumber++}</p>
+                          <img
+                            src={`
+                                 ${collection?.cover_image?.includes('/ipfs') ||
+                                collection?.cover_image?.includes('ipfs://')
+                                ? getImage(collection?.cover_image)
+                                : collection?.cover_image
+                                  ? collection?.cover_image
+                                  : user
+                              }
+                                
+                               `}
+                            alt="collection"
+                          />
+                        </div>
+                        <div className={style.tpRight}>
+                          <div className={style.tprInfo}>
+                            <h2>{collection?.title || 'Untitled'}</h2>
+                            <p>Floor price: 0</p>
+                          </div>
+                          <div className={style.tprNumbers}>
+                            <p>+30</p>
+                            <p>Vol: 0</p>
+                          </div>
+                        </div>
+
+                      </Link>
+                    )
+                  )
+                })}
+
             </div>
           </div>
         </div>
