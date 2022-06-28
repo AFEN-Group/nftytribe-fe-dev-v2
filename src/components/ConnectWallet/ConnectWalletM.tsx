757109be23632b3eec4ab3e935f78fd4adc115d2
AyoNiyi <mac@Ayos-Mac.local>
2022-06-28 06:21:54 +0100

    update: allow only rinkeby for testing

--- src/components/ConnectWallet/ConnectWalletM.tsx
+++ src/components/ConnectWallet/ConnectWalletM.tsx
@@ -409,12 +409,12 @@ const ConnectWalletM = (props: any) => {
                                 <div className={style.bodyTxt}>
                                     <p className={`${dark === 'true' ? style.b1 : style.b2}`}>
                                         Connect with one of our wallet providers or{' '}
-                                        <span
+                                        {/* <span
                                             className={`${dark === 'true' ? 'yellowMain' : 'blueLight'
                                                 }`}
-                                        >
-                                            Create
-                                        </span>{' '}
+                                        > */}
+                                        Create{' '}
+                                        {/* </span>{' '} */}
                                         a new one
                                     </p>
                                 </div>
