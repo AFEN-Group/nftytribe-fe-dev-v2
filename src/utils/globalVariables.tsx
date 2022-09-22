472cd055fa7accbb0a6f1e2fbcf2e46fe07f2204
victor khachi <victorkhachi@gmail.com>
2022-09-23 00:24:48 +0100

    update

--- src/utils/globalVariables.tsx
+++ src/utils/globalVariables.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 export default {
-    baseURL: 'https://api.nftytribe.io/api',
+    baseURL: 'https://dev.api.nftytribe.io/api',
     mainnetEth: {
         chain: 'ethereum',
         chainId: '0x1',
@@ -18,3 +18,23 @@ export default {
         chainId: '0x61',
     }
 }
+
+// export default {
+//     baseURL: 'https://dev.api.nftytribe.io/api',
+//     mainnetEth: {
+//         chain: 'ethereum',
+//         chainId: '0x1',
+//     },
+//     testnetEth: {
+//         chain: 'rinkeby',
+//         chainId: '0x4',
+//     },
+//     mainnetBsc: {
+//         chain: 'bsc',
+//         chainId: '0x38',
+//     },
+//     testnetBsc: {
+//         chain: 'bsc testnet',
+//         chainId: '0x61',
+//     }
+// }
