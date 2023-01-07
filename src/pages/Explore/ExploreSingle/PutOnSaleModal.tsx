93ae5d78c472f69868156bb22b71d67625e0cbae
supermankachi <vobi@dedukt.co>
2023-01-07 19:08:19 +0100

    update

--- src/pages/Explore/ExploreSingle/PutOnSaleModal.tsx
+++ src/pages/Explore/ExploreSingle/PutOnSaleModal.tsx
@@ -80,9 +80,11 @@ const PutOnSaleModal = (props: any) => {
             
                 try {
                    console.log(props.id,onsaleParams.amount);
-                  const  decimal=parseInt(await token.methods.decimals().call())
+                  const  decimal=parseInt(await token.methods.decimals().call({from:wallet_address}))
                   let amount = parseInt(onsaleParams.amount)*10**decimal
-                    await marketPlaceContract.methods.putOnSale(props.id,amount,onsaleParams.marketType,onsaleParams.from,onsaleParams.to,props.collectionAddress,onsaleParams.erc20).send({from:wallet_address})
+                  console.log();
+                  
+                    await marketPlaceContract.methods.putOnSale(props.id,JSON.stringify(amount),onsaleParams.marketType,onsaleParams.from,onsaleParams.to,props.collectionAddress,onsaleParams.erc20).send({from:wallet_address})
                     setCompleted(!completed)
                 } catch (error) {
 
